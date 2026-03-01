c76096b94b0f5c806e5f15bf5f31a868125f6be2
Meet Parmar <meetpparmar34@gmail.com>
2026-03-01 17:26:06 +0530

    small server issue fixed

--- server/main.py
+++ server/main.py
@@ -93,4 +93,5 @@ def predict():
         }), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
